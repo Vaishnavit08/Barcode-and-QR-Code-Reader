c108e1b469e91e0cf9b9f24dfb3aea13243a2787
Vaishnavit08 <92875011+Vaishnavit08@users.noreply.github.com>
2025-02-05 22:54:24 +0530

    Add files via upload

---
+++ qr.py
@@ -0,0 +1,19 @@
+def vaish(name):
+    a=list(name)
+
+    x=0
+
+    
+
+    for i in a:
+        if i == " ":
+            continue
+        else:
+          x=x+1
+    print(x)
+
+    
+
+
+
+print(vaish("vaishnavii talekar"))
